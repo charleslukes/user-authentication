d1669908aa5249a985394086af8de516f9f3c9e7
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-11 18:55:06 +0100

    add the models and routes for server

---
+++ server/models/register.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+
+const Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 255
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  confirm: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  profilePic: {
+    type: String,
+    required: true,
+  }
+})
+
+export const validation = (body) => {
+  const schema = {
+    name: Joi.string().min(3).max(255).required(),
+    email: Joi.string().min(3).max(255).email().required(),
+    username: Joi.string().min(3).max(255).required(),
+    password: Joi.string().min(5).max(255).required(),
+    confirm: Joi.string().min(5).max(255).required(),
+    profilePic: Joi.string().required()
+  }
+
+  return Joi.validate(body, schema)
+}
+
+export const user = mongoose.model('User', Schema);
