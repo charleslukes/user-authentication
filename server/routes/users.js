ec8679cdb3b41694adc8169c1cacc4212342efe9
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-11 23:36:24 +0100

    add the routes to register users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const {register} = require('../controllers/register');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/api/register', (req, res) => {
+router.post('/api/register', register);
 
-})
 module.exports = router;
