3e038653ff2082e06d31e747e105d3ad0fc3ddc9
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-13 12:17:33 +0100

    update the routes for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {register} = require('../controllers/register');
+const {Login} = require('../controllers/login');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/api/register', register);
+router.post('/api/register', register)
+      .post('/api/login', Login)
 
 module.exports = router;
