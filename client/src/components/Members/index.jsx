5098a6e04dd7aac2adce6f6aada869522a8bb8a2
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-13 12:06:21 +0100

    add the members page

---
+++ client/src/components/Members/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Members = () => {
+
+  return (
+    <>
+      <h5>Hello Welcome to the Members Page, You are now a member</h5>
+    </>
+  )
+}
+
+export default Members;
